278f1ab54dbc679c0b2ca87363e0e29cc9898269
ammeLacy <emma_lacy@hotmail.com>
2019-09-14 18:42:03 +0100

    Remove console.logs

--- test/testhooks.spec.js
+++ test/testhooks.spec.js
@@ -5,12 +5,10 @@ const User = require('../models/userModel');
 const Country = require('../models/countryModel')
 
 beforeEach((done) => {
-  console.log('beforeEach start')
   const dropCountries = Country.deleteMany({});
   const dropUsers = User.deleteMany({});
   Promise.all([dropCountries, dropUsers])
     .then(() => {
-      console.log('beforeEach cleared data')
       const addUser = new User({
         name: 'John Doe',
         email: 'test@gmail.com',
@@ -35,7 +33,6 @@ beforeEach((done) => {
         .save();
       Promise.all([addUser, addScotland, addSouthAfrica])
         .then(() => {
-          console.log('beforeEach added data')
           done()
         });
     });

e36a3b9df702fa5420c41461f6ebbf713404c8c5
ammeLacy <emma_lacy@hotmail.com>
2019-09-16 21:05:12 +0100

    used sharp and file system

--- models/tesseractModel.js
+++ models/tesseractModel.js
@@ -1,4 +1,5 @@
 const tesseract = require("node-tesseract-ocr");
+const sharp = require('sharp');
 
 const config = {
   lang: "eng",
@@ -7,15 +8,23 @@ const config = {
 };
 
 const readImage = ({ photo }) => {
-  return tesseract
-    .recognize(photo, config)
-    .then(text => {
-      console.log(text, "<-- in model");
-      return text;
-    })
-    .catch(err => {
-      console.log("error:", err);
-    });
+  var img = new Buffer(photo, 'base64');
+  return sharp(img)
+    .resize(700, 450)
+    .greyscale()
+    .sharpen(2)
+    .toFile("out2.png").then(() => {
+      return tesseract
+        .recognize("out2.png", config)
+        .then(text => {
+          console.log(text, "<-- in model");
+          return text;
+        })
+        .catch(err => {
+          console.log("error:", err);
+        });
+    }
+    ).catch(err => console.log("sharp ", err));
 };
 
 module.exports = readImage;
